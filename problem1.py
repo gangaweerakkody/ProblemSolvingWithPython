ddfc04e33ac2f533ac557acf1221c7e93afc9ddc
gangaweerakkody <127628647+gangaweerakkody@users.noreply.github.com>
2025-01-22 18:20:54 +0530

    Problem1,2,3

---
+++ problem1.py
@@ -0,0 +1,13 @@
+
+# find average of N numbers in python
+
+num=int(input("How many numbers ?"))
+total_sum=0
+
+for n in range(num) :
+    numbers=float(input("Enter any number"))
+    total_sum += numbers
+
+avg= total_sum / num
+
+print("Average is :", avg)
---
+++ problem2.py
@@ -0,0 +1,5 @@
+# how to sum of the first n positive integers in python
+n= int(input("Enter number: "))
+
+total_sum=(n*(n+1)) / 2
+print(total_sum)
