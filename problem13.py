3476d37b3c03ba4b2f026d80ed1681677e170214
gangaweerakkody <127628647+gangaweerakkody@users.noreply.github.com>
2025-02-06 15:20:29 +0530

    Create problem13.py

---
+++ problem13.py
@@ -0,0 +1,18 @@
+number= int(input("Enter a number: "))
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+
+
+
+n= int(input('Enter a number'))
+
+if (1<=n<=100):
+    if (n % 2==0) :
+        print('Not Weird')
+    else:
+        print('Weird')
+        
